85f8c668e93a0b9e987cf2ee4f3f24e4d783df3e
DIlshan Madhawa <dilmadhawa@Compaq-620.(none)>
2013-10-15 23:29:04 +0530

    Before starting second development phase

---
+++ src/com/immanent/user/User.java
@@ -0,0 +1,20 @@
+package com.immanent.user;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.immanent.services.ServiceController;
+
+/**
+ * Servlet implementation class user
+ */
+public class User extends ServiceController {
+	private static final long serialVersionUID = 1L;
+ 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		dispatch("/home.jsp", request, response);
+	}
+
+}
